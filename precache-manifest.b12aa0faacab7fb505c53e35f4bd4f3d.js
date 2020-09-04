be4b5ab73d96e09de5002e3d0cd8029fbf4eabdd
Phonon <8019825+phonon_QAQ@user.noreply.gitee.com>
2020-09-04 17:00:04 +0800

    deploy

---
+++ precache-manifest.b12aa0faacab7fb505c53e35f4bd4f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49bb842d6a3f9deddfe2840f744bb62",
+    "url": "/XiaoKaiCounting-website/QRcode.png"
+  },
+  {
+    "revision": "428929249247a4ea600f",
+    "url": "/XiaoKaiCounting-website/css/app.5511d100.css"
+  },
+  {
+    "revision": "b31f3801965ac155bfb4ae102846218d",
+    "url": "/XiaoKaiCounting-website/index.html"
+  },
+  {
+    "revision": "428929249247a4ea600f",
+    "url": "/XiaoKaiCounting-website/js/app.14e8d982.js"
+  },
+  {
+    "revision": "aef60b17dd4cffd29528",
+    "url": "/XiaoKaiCounting-website/js/chunk-vendors.ada22c2d.js"
+  },
+  {
+    "revision": "124e2d289a2fe5170ac3544f0e65b5ce",
+    "url": "/XiaoKaiCounting-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/XiaoKaiCounting-website/robots.txt"
+  }
+]);
